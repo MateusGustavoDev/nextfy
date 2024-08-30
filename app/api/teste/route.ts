90709b89e23e4d185a92e9012cce503977fcded7
Mateus Gustavo <mateusgustavodev@gmail.com>
2024-08-29 22:06:48 -0300

    feat: add runtime

--- app/api/teste/route.ts
+++ app/api/teste/route.ts
@@ -1,6 +1,8 @@
 import { headers } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
+export const runtime = 'nodejs'
+
 export async function GET() {
   return NextResponse.json(
     { message: `Hello from ${process.env.VERCEL_REGION}` },
