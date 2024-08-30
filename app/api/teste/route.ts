4748bac6707048dbc4de95349483fa498c284ee2
Mateus Gustavo <mateusgustavodev@gmail.com>
2024-08-29 22:29:20 -0300

    test 2

--- app/api/teste/route.ts
+++ app/api/teste/route.ts
@@ -1,11 +1,3 @@
-import { headers } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
-
-export const runtime = 'edge'
-
-export async function GET() {
-  return NextResponse.json(
-    { message: `Hello from ${process.env.VERCEL_REGION}` },
-    { headers: { 'Cache-Control': 'max-age=10' }, status: 200 },
-  )
+export function GET(request: Request) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`)
 }
