246bd69d436c702cc3285cb1fc848b7bbde0123a
Mateus Gustavo <mateusgustavodev@gmail.com>
2024-08-30 22:39:17 -0300

    teste edge runtime

--- app/api/teste/route.ts
+++ app/api/teste/route.ts
@@ -1,6 +1,8 @@
 import { getEnv } from '@vercel/functions'
 
+export const runtime = 'edge'
+
 export async function GET() {
   const { VERCEL_REGION } = getEnv()
-  return new Response(`Hello from ${VERCEL_REGION}`)
+  return new Response(`Hello from ${VERCEL_REGION}`, { status: 200 })
 }
