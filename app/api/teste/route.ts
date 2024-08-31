3927d7f082f204e98f14880f7602481ee789ef9b
Mateus Gustavo <mateusgustavodev@gmail.com>
2024-08-30 22:54:38 -0300

    test gru1 edge

--- app/api/teste/route.ts
+++ app/api/teste/route.ts
@@ -2,6 +2,8 @@ import { getEnv } from '@vercel/functions'
 
 export const runtime = 'edge'
 
+export const preferredRegion = ['gru1', 'iad1']
+
 export async function GET() {
   const { VERCEL_REGION } = getEnv()
   return new Response(`Hello from ${VERCEL_REGION}`, { status: 200 })
